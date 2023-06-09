55d3e2879c309ea71858dfc8b917dbba273e3506
cxy_xk <724410870@qq.com>
2023-06-09 12:27:18 +0800

    取消

--- app/src/main/java/com/xk/application/GuestFragment.java
+++ app/src/main/java/com/xk/application/GuestFragment.java
@@ -4,6 +4,7 @@ package com.xk.application;
   @Author xk @Date 2023/04/18 23:42
  */
 
+import android.annotation.SuppressLint;
 import android.icu.util.Calendar;
 import android.os.Build;
 import android.os.Bundle;
@@ -28,7 +29,6 @@ public class GuestFragment extends Fragment {
     // Browser实体集合
     private final List<Browser> browsers = new ArrayList<>();
     BrowserAdapter adapter;
-    private long seed = 0;
     TzCacul tzCacul = new TzCacul();
     int cycle = 0;
 
@@ -36,7 +36,7 @@ public class GuestFragment extends Fragment {
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_guest, null);
+        @SuppressLint("InflateParams") View view = inflater.inflate(R.layout.activity_guest, null);
         // 初始化数据
         final int[] imageId = new int[]{R.drawable.ic_d3, R.drawable.ic_s3, R.drawable.ic_d4, R.drawable.ic_s4, R.drawable.ic_solid};
         final String[] check = new String[]{"不提供", "错", "对"};
@@ -45,7 +45,7 @@ public class GuestFragment extends Fragment {
         ListView listView = view.findViewById(R.id.list_view);
         listView.setTranscriptMode(AbsListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
         listView.setAdapter(adapter);
-        final TextView tvcurrent = view.findViewById(R.id.tvcurrent);
+        final TextView tvCurrent = view.findViewById(R.id.tvcurrent);
         Button g_d3 = view.findViewById(R.id.g_d3);
         Button g_s3 = view.findViewById(R.id.g_s3);
         Button g_d4 = view.findViewById(R.id.g_d4);
@@ -59,16 +59,16 @@ public class GuestFragment extends Fragment {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                 cycle = (ca.get(Calendar.HOUR) * 60) + (ca.get(Calendar.MINUTE) + 1);
             }
-            String datet = null;
+            String dated = null;
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                datet = "第" + ca.get(Calendar.YEAR) + "0" + ((ca.get(Calendar.MONTH)) + 1) + ca.get(Calendar.DATE) + cycle + "期";
+                dated = "第" + ca.get(Calendar.YEAR) + "0" + ((ca.get(Calendar.MONTH)) + 1) + ca.get(Calendar.DATE) + cycle + "期";
             }
             tzCacul.getTzList().add(new TZData("<font color='#0072E3'>" + getString(R.string.d3) + "</font>", getString(R.string.you), 1, 2, 3));
 
             tzCacul.initDSValueT();
-            adapter.add(new Browser(datet, imageId[yuced3()], R.drawable.ic_solid, R.drawable.ic_d3, check[checkD3()]));
+            adapter.add(new Browser(dated, imageId[yuCeD3()], R.drawable.ic_solid, R.drawable.ic_d3, check[checkD3()]));
             if (tzCacul.currentData().size() > 0) {
-                tvcurrent.setText(tzCacul.currentData().get(tzCacul.currentData().size() - 1));
+                tvCurrent.setText(tzCacul.currentData().get(tzCacul.currentData().size() - 1));
             }
         });
         g_s3.setOnClickListener(view12 -> {
@@ -80,40 +80,40 @@ public class GuestFragment extends Fragment {
                 cycle = (ca.get(Calendar.HOUR) * 60) + (ca.get(Calendar.MINUTE) + 1);
             }
             assert ca != null;
-            String datet = "第" + ca.get(Calendar.YEAR) + "0" + ((ca.get(Calendar.MONTH)) + 1) + ca.get(Calendar.DATE) + cycle + "期";
+            String dated = "第" + ca.get(Calendar.YEAR) + "0" + ((ca.get(Calendar.MONTH)) + 1) + ca.get(Calendar.DATE) + cycle + "期";
 
 
             tzCacul.getTzList().add(new TZData("<font color='#ff0000'>" + getString(R.string.s3) + "</font>", getString(R.string.zuo), 2, 1, 3));
             tzCacul.initDSValueT();
-            adapter.add(new Browser(datet, imageId[yuced3()], R.drawable.ic_solid, R.drawable.ic_s3, check[checkS3()]));
+            adapter.add(new Browser(dated, imageId[yuCeD3()], R.drawable.ic_solid, R.drawable.ic_s3, check[checkS3()]));
             if (tzCacul.currentData().size() > 0) {
-                tvcurrent.setText(tzCacul.currentData().get(tzCacul.currentData().size() - 1));
+                tvCurrent.setText(tzCacul.currentData().get(tzCacul.currentData().size() - 1));
             }
         });
         g_d4.setOnClickListener(view13 -> {
             Calendar ca = Calendar.getInstance(Locale.US);
             cycle = (ca.get(Calendar.HOUR) * 60) + (ca.get(Calendar.MINUTE) + 1);
-            String datet = "第" + ca.get(Calendar.YEAR) + "0" + ((ca.get(Calendar.MONTH)) + 1) + ca.get(Calendar.DATE) + cycle + "期";
+            String dated = "第" + ca.get(Calendar.YEAR) + "0" + ((ca.get(Calendar.MONTH)) + 1) + ca.get(Calendar.DATE) + cycle + "期";
 
 
             tzCacul.getTzList().add(new TZData("<font color='#0072E3'>" + getString(R.string.d4) + "</font>", getString(R.string.zuo), 1, 1, 4));
             tzCacul.initDSValueT();
-            adapter.add(new Browser(datet, imageId[yuced3()], R.drawable.ic_solid, R.drawable.ic_d4, check[checkD4()]));
+            adapter.add(new Browser(dated, imageId[yuCeD3()], R.drawable.ic_solid, R.drawable.ic_d4, check[checkD4()]));
             if (tzCacul.currentData().size() > 0) {
-                tvcurrent.setText(tzCacul.currentData().get(tzCacul.currentData().size() - 1));
+                tvCurrent.setText(tzCacul.currentData().get(tzCacul.currentData().size() - 1));
             }
 
         });
         g_s4.setOnClickListener(view14 -> {
             Calendar ca = Calendar.getInstance(Locale.US);
             cycle = (ca.get(Calendar.HOUR) * 60) + (ca.get(Calendar.MINUTE) + 1);
-            String datet = "第" + ca.get(Calendar.YEAR) + "0" + ((ca.get(Calendar.MONTH)) + 1) + ca.get(Calendar.DATE) + cycle + "期";
+            String dated = "第" + ca.get(Calendar.YEAR) + "0" + ((ca.get(Calendar.MONTH)) + 1) + ca.get(Calendar.DATE) + cycle + "期";
 
             tzCacul.getTzList().add(new TZData("<font color='#ff0000'>" + getString(R.string.s4) + "</font>", getString(R.string.you), 2, 2, 4));
             tzCacul.initDSValueT();
-            adapter.add(new Browser(datet, imageId[yuced3()], R.drawable.ic_solid, R.drawable.ic_s4, check[checkS4()]));
+            adapter.add(new Browser(dated, imageId[yuCeD3()], R.drawable.ic_solid, R.drawable.ic_s4, check[checkS4()]));
             if (tzCacul.currentData().size() > 0) {
-                tvcurrent.setText(tzCacul.currentData().get(tzCacul.currentData().size() - 1));
+                tvCurrent.setText(tzCacul.currentData().get(tzCacul.currentData().size() - 1));
             }
         });
         g_ht.setOnClickListener(view15 -> {
@@ -126,7 +126,7 @@ public class GuestFragment extends Fragment {
         return view;
     }
 
-    public int yuced3() {
+    public int yuCeD3() {
         int id = 0;
         if (tzCacul.currentData().size() > 0) {
             for (int i = 0; i < tzCacul.currentData().size() - 1; i++) {
